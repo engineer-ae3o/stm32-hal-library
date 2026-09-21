6e462474167992bb898fafa4824a6922a3642aa3
Akemu Elisha Enorhor <akemuelisha07@gmail.com>
2026-09-21 13:37:33 +0100

    Also, if you couldn't tell, that was sarcasm. Iykyk

--- components/utils/src/common.c
+++ components/utils/src/common.c
@@ -3,7 +3,7 @@
 #include "utils/log.h"
 
 
-[[__gnu__::__noreturn__]] void halt(void) {
+void halt(void) {
     __disable_irq();
 #if defined(DEBUG)
     __BKPT(0);
@@ -13,19 +13,19 @@
     }
 }
 
-void assert_check(bool cond, const char* msg, const char* function, const char* file, int line) {
-    if (gnu_unlikely(!cond)) {
-        LOGE("Assert", "Assert (%s) failed", msg);
-        panic(function, file, line);
-    }
-}
-
 void panic(const char* function, const char* file, int line) {
-    LOGE("Panic", "System ran into a fatal error from %s (%s:%d)", function, file, line);
+    LOGE("Panic", "Fatal error from %s (%s:%d)", function, file, line);
     halt();
 }
 
 void restart(const char* function, const char* file, int line) {
     LOGI("Restart", "System reboot requested from %s (%s:%d)", function, file, line);
     NVIC_SystemReset();
+}
+
+void assert_check(bool cond, const char* msg, const char* function, const char* file, int line) {
+    if (gnu_unlikely(!cond)) {
+        LOGE("Assert", "Assert (%s) failed", msg);
+        panic(function, file, line);
+    }
 }
